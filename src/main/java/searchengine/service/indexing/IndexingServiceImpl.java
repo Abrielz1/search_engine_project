6121943dec7801cb1ebe0c01f6cf50d761ec1533
Abriel <abrielz11@gmail.com>
2024-05-31 13:09:22 +0300

    added thread name in beginIndexingSites()

--- src/main/java/searchengine/service/indexing/IndexingServiceImpl.java
+++ src/main/java/searchengine/service/indexing/IndexingServiceImpl.java
@@ -123,6 +123,7 @@ public class IndexingServiceImpl implements IndexingService {
         pool = new ForkJoinPool();
         List<Thread> threads = new ArrayList<>();
         List<Site> siteList = siteRepository.findAll();
+        int tread = 0;
 
         for (Site site : siteList) {
             threads.add(new Thread(() -> {
@@ -134,7 +135,7 @@ public class IndexingServiceImpl implements IndexingService {
                         manipulator));
 
                 this.setSitesIndexedStatus(site);
-            }));
+            }, "tread" + tread++));
         }
         threads.forEach(Thread::start);
     }
